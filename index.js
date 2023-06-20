5c9143c4f479ba7ff34900404ead32cf93a3627f
Edgar Nwajei <edgarlinked@gmail.com>
2023-06-20 17:44:36 +0100

    declared variables

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 const currentTime = document.querySelector("h1");
+const content =  document.querySelector("time-set");
+const selectMenu =  document.querySelectorAll("input");
+const setAlarmBtn =   document.querySelector("btn");
+
+
